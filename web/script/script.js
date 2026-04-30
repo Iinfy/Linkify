f96f61cadf3436e15388ad10ba7f725a4c99d2d8
kommo124 <negrilla32@yandex.ru>
2026-04-30 21:14:31 +0300

    feat: sending url from linkInput to endpoint

--- web/script/script.js
+++ web/script/script.js
@@ -4,16 +4,20 @@ const shortBtn = document.getElementById("shortBtn")
 shortBtn.addEventListener("click", (e) => {
     e.preventDefault()
     const url = linkInput.value.trim()
+    shortBtn.textContent = "shorted"
+    shortBtn.style.backgroundColor = 'green';
 
     try {
-        const response = await fetch("/slink", {
+        const response = await fetch(`${ENV.API_URL}/slink`, {
+            
+    
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({url: url})
         })
-        
+
         const data = await response.json()
     } catch(error) {
         console.error(error)
